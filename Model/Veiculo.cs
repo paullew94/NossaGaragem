cb222f53ce1242aa9ceef441a80a4a1b2cd2d5b5
paullew94 <paullew@hotmail.com>
2019-07-11 11:29:22 -0300

    Adocionado a bibloteca do bootstrap font awesome desenvolvimento do layout master e lista de veiculos

---
+++ Model/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Veiculo
+    {
+        public int Id;
+        public string Modelo;
+        public decimal Valor;
+
+        //propiedade para a coluna id _categoria (FK)
+        public int IdCategoria;
+        
+        //objeto da categoria que permitiria acessar as informaçoes da categoria atraves do veiculo
+        public Categoria Categoria;
+
+    }
+}
